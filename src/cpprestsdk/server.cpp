7660076853b68bbb23649980ec608dab6240a77b
Yahor Zabalotski <yzabalotski@gmail.com>
2018-10-07 00:49:19 +0300

    Compile and run example from https://mariusbancila.ro/blog/2017/11/19/revisited-full-fledged-client-server-example-with-c-rest-sdk-2-10/

--- src/cpprestsdk/server.cpp
+++ src/cpprestsdk/server.cpp
@@ -1,7 +1,35 @@
+#include <cpprest/http_listener.h>
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
 #include <iostream>
+using namespace std;
+
+#define PRINT(msg) wcout << msg
+
+void handle_get(http_request request)
+{
+	PRINT("\nhandle GET\n");
+	request.reply(status_codes::OK, "hello, world!");
+}
 
 int main(void)
 {
-	std::cout << "hello, world!\n";
+	http_listener listener("http://localhost:9999/restdemo");
+	listener.support(methods::GET, handle_get);
+
+	try {
+		listener.open()
+			.then([&listener]() { PRINT("\nstarting to listen\n"); })
+			.wait();
+
+		while (true);
+	}
+	catch (exception const & e) {
+		wcout << e.what() << endl;
+	}
+
 	return 0;
 }
